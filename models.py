c80ee7a6367052ab66dd76307f5a72054d1a37f2
Miriam Cordes <miriam.cordes@intive-kupferwerk.com>
2021-09-10 20:33:16 +0200

    change type in movie model

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, create_engine
+from sqlalchemy import Column, Integer, String, Date, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import os
 import json
@@ -26,7 +26,7 @@ class Movie(db.Model):
 
     id=Column(Integer, primary_key=True)
     title=Column(String, nullable=False)
-    release_date=Column(DateTime, nullable=False)
+    release_date=Column(Date, nullable=False)
 
     def __init__(self, title, release_date):
         self.title = title
